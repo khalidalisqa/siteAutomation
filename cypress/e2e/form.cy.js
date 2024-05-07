3286d80135a0e0602843d143e86a70faf40d276d
khalidalisqa <khalidalisqa@gmail.com>
2024-05-07 16:04:41 +0500

    write code for cookies and send keys in form

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,5 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
+    // Clears cookies for the current domain
+    cy.clearCookies();
+    // Set viewport width and height
+    cy.viewport(1280, 720);
     cy.visit('https://demoqa.com/');
+    cy.contains('[class="card-body"] h5', 'Forms').click();
+    cy.contains('#item-0', 'Practice Form').click();
+    cy.get('#firstName').type("Ali");
   })
 })
