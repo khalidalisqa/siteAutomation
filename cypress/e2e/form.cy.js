a924bda2c93405dd4d3ae2edd101b0d0b8bed263
khalidalisqa <khalidalisqa@gmail.com>
2024-05-07 16:29:27 +0500

    write code for date of birthday and phone

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -8,5 +8,10 @@ describe('template spec', () => {
     cy.contains('[class="card-body"] h5', 'Forms').click();
     cy.contains('#item-0', 'Practice Form').click();
     cy.get('#firstName').type("Ali");
+    cy.get('#lastName').type("shan");
+    cy.get('#userEmail').type("upmail@gmail.com");
+    cy.get('#userNumber').type("03009988776");
+    cy.get('#dateOfBirthInput').type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}');
+    cy.get('#dateOfBirthInput').type("2 DEC 1995");
   })
 })
