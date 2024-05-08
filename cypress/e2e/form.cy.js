b0289c3a9d4cc640e4962c572cf554faa7958da2
khalidalisqa <khalidalisqa@gmail.com>
2024-05-08 12:54:01 +0500

    write script for xpath automation

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress-xpath" />
 describe('template spec', () => {
   it('passes', () => {
     // Clears cookies for the current domain
@@ -6,7 +7,8 @@ describe('template spec', () => {
     cy.viewport(1280, 720);
     cy.visit('https://demoqa.com/');
     cy.contains('[class="card-body"] h5', 'Forms').click();
-    cy.contains('#item-0', 'Practice Form').click();
+    cy.xpath('//span[text()="Practice Form"]').click({force:true});
+    //cy.contains('#item-0', 'Practice Form').click();
     cy.get('#firstName').type("Ali");
     cy.get('#lastName').type("shan");
     cy.get('#userEmail').type("upmail@gmail.com");
